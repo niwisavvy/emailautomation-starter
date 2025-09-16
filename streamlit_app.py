f0137f8bab24ae808d0b41bd5a0360631229cfff
niwisavvy <139376783+niwisavvy@users.noreply.github.com>
2025-09-16 17:43:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,9 +163,9 @@ if send_clicked:
         if st.session_state.get("stop_sending", False):
             st.warning("Email sending stopped by user.")
             break
-st.session_state.sending = True
-st.session_state.stop_sending = False
-st.session_state.sent_count = 0
+    st.session_state.sending = True
+    st.session_state.stop_sending = False
+    st.session_state.sent_count = 0
     progress = st.progress(0)
     total = len(df)
     sent = 0
